a046fd6f15052e6e1aae1f1a189c68407e3032bd
Alex Kapajika <alex.kapajika.ntumba@stu.ocsb.ca>
2023-09-19 15:57:29 +0000

    added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created By: Alex Kapajika
+# Date: Sep. 19, 2023
+# This program displays hello world with style.
+
+
+def main():
+    print("Hello, Ms Raffin")
+    print("Programming is so interesting!")
+
+
+if __name__ == "__main__":
+    main()
